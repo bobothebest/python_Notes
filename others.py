6f6f5c134a74c3fd13aa3146abbb2d4cb5b0a5a5
bobo <421328026@qq.com>
2023-07-19 10:44:55 +0800

    Create others.py

---
+++ others.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 19 09:36:23 2023
+
+@author: zzy
+杂项，这里放一些通用常用命令
+"""
+#查看数据类型
+type(111)
+type(1.234)
+type('hello')
+type([1,2,3,4])
+
